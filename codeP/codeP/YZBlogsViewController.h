0335c526d93cfa8f928373a3ea64a5978ac6efdb
Yueng <markyueng@AppledeMacBook-Pro-2.local>
2016-07-19 23:16:15 +0900

    oc version
    delete swift and add oc version

---
+++ codeP/codeP/YZBlogsViewController.h
@@ -0,0 +1,13 @@
+//
+//  YZBlogsViewController.h
+//  codeP
+//
+//  Created by MarkYueng on 16/7/19.
+//  Copyright © 2016年 MarkYueng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YZBlogsViewController : UITabBarController
+
+@end
---
+++ codeP/codeP/YZMainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  YZMainTabBarController.h
+//  codeP
+//
+//  Created by MarkYueng on 16/7/19.
+//  Copyright © 2016年 MarkYueng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YZMainTabBarController : UITabBarController
+
+@end
